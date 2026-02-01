0921f707b7146fcf987d8a145457c6ffbe8d36b0
Zach Short <zacharyfshort@gmail.com>
2026-02-01 16:33:43 +0000

    Built my app!

---
+++ clerk-nextjs/app/page.tsx
@@ -0,0 +1,19 @@
+import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <SignedOut>
+        <h1>You are signed out</h1>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+
+      <SignedIn>
+        <h1>You are signed in</h1>
+        <UserButton />
+      </SignedIn>
+    </main>
+  )
+}
+
